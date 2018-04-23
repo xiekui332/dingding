e8c30ac848e5421b870d9939914f275b7d97ac60
zhuyl <zhuyl@cnjiang.com>
2018-04-23 15:34:30 +0800

    个人中信的修改

--- src/asset/js/userCenter.js
+++ src/asset/js/userCenter.js
@@ -7,38 +7,50 @@ var vm = new Vue({
     data: {
       navList:[
           {
-            url:'/src/asset/images/icon/waitPay.png',
+            url:'/src/asset/images/icon/wait_pay.png',
             navTitle:'待支付' 
           },
           {
-            url:'/src/asset/images/icon/waitPay.png',
+            url:'/src/asset/images/icon/wait_deliver.png',
             navTitle:'待发货' 
           },
           {
-            url:'/src/asset/images/icon/waitPay.png',
+            url:'/src/asset/images/icon/wait_receive.png',
             navTitle:'待收货' 
           },
           {
-            url:'/src/asset/images/icon/waitPay.png',
+            url:'/src/asset/images/icon/review_success.png',
             navTitle:'租凭中' 
           },
           {
-            url:'/src/asset/images/icon/waitPay.png',
+            url:'/src/asset/images/icon/review_success.png',
             navTitle:'租凭中' 
-          }
+          },
+          {
+            url:'/src/asset/images/icon/review_success.png',
+            navTitle:'租凭中' 
+          },
+          {
+            url:'/src/asset/images/icon/review_success.png',
+            navTitle:'租凭中' 
+          },
+          {
+            url:'/src/asset/images/icon/review_success.png',
+            navTitle:'租凭中' 
+          },
 
       ],
       mainList:[
           {
-              urll:'/src/asset/images/icon/wenhao.png',
+              urll:'/src/asset/images/icon/wenhao (2).png',
               mainTitle:'我的账单'
           },
           {
-            urll:'/src/asset/images/icon/zan.png',
+            urll:'/src/asset/images/icon/review_success.png',
             mainTitle:'我的授权'
         },
         {
-            urll:'/src/asset/images/icon/wenhao.png',
+            urll:'/src/asset/images/icon/wenhao (2).png',
             mainTitle:'关于我们'
         },
       ]  
@@ -50,6 +62,9 @@ var vm = new Vue({
     filters: {
     },
     methods: {
+    //   myFunction() {
+    //     document.getElementById("demo").innerHTML = x += 1;
+    //  }
     },
     created() {
     },
