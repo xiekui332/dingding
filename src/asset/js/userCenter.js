fc7d85ef58af6410accaa32ea2d73f4f164f3310
zhuyl <zhuyl@cnjiang.com>
2018-04-23 14:27:11 +0800

    首页和个人中心的提交

--- src/asset/js/userCenter.js
+++ src/asset/js/userCenter.js
@@ -5,6 +5,43 @@ var vm = new Vue({
     props: {
     },
     data: {
+      navList:[
+          {
+            url:'/src/asset/images/icon/waitPay.png',
+            navTitle:'待支付' 
+          },
+          {
+            url:'/src/asset/images/icon/waitPay.png',
+            navTitle:'待发货' 
+          },
+          {
+            url:'/src/asset/images/icon/waitPay.png',
+            navTitle:'待收货' 
+          },
+          {
+            url:'/src/asset/images/icon/waitPay.png',
+            navTitle:'租凭中' 
+          },
+          {
+            url:'/src/asset/images/icon/waitPay.png',
+            navTitle:'租凭中' 
+          }
+
+      ],
+      mainList:[
+          {
+              urll:'/src/asset/images/icon/wenhao.png',
+              mainTitle:'我的账单'
+          },
+          {
+            urll:'/src/asset/images/icon/zan.png',
+            mainTitle:'我的授权'
+        },
+        {
+            urll:'/src/asset/images/icon/wenhao.png',
+            mainTitle:'关于我们'
+        },
+      ]  
     },
     computed: {
     },
