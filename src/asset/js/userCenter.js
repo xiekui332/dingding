b4053f459140088fcc694fa4b1f4725b0b5f1ba3
zhuyl <zhuyl@cnjiang.com>
2018-04-24 10:35:16 +0800

    个人中心的修改

--- src/asset/js/userCenter.js
+++ src/asset/js/userCenter.js
@@ -5,10 +5,11 @@ var vm = new Vue({
     props: {
     },
     data: {
+      show:true,
       navList:[
           {
-            url:'/src/asset/images/icon/wait_pay.png',
-            navTitle:'待支付' 
+           url:'/src/asset/images/icon/review_success.png',
+           navTitle:'租用中' 
           },
           {
             url:'/src/asset/images/icon/wait_deliver.png',
@@ -19,38 +20,49 @@ var vm = new Vue({
             navTitle:'待收货' 
           },
           {
-            url:'/src/asset/images/icon/review_success.png',
-            navTitle:'租凭中' 
+            url:'/src/asset/images/icon/wait_pay.png',
+            navTitle:'待支付' 
           },
           {
-            url:'/src/asset/images/icon/review_success.png',
-            navTitle:'租凭中' 
+            url:'/src/asset/images/icon/in_review.png',
+            navTitle:'审核中' 
           },
-          {
-            url:'/src/asset/images/icon/review_success.png',
-            navTitle:'租凭中' 
-          },
-          {
-            url:'/src/asset/images/icon/review_success.png',
-            navTitle:'租凭中' 
-          },
-          {
-            url:'/src/asset/images/icon/review_success.png',
-            navTitle:'租凭中' 
-          },
+          
 
       ],
-      mainList:[
-          {
-              urll:'/src/asset/images/icon/wenhao (2).png',
-              mainTitle:'我的账单'
-          },
-          {
-            urll:'/src/asset/images/icon/review_success.png',
-            mainTitle:'我的授权'
+      navList2:[
+        {
+          url:'/src/asset/images/icon/in_review.png',
+          navTitle:'审核中' 
         },
         {
-            urll:'/src/asset/images/icon/wenhao (2).png',
+          url:'/src/asset/images/icon/review_success.png',
+          navTitle:'审核通过' 
+        },
+        {
+          url:'/src/asset/images/icon/review_fail.png',
+          navTitle:'审核失败' 
+        },
+        {
+          url:'/src/asset/images/icon/lease_full.png',
+          navTitle:'租期已满' 
+        },
+        {
+          url:'/src/asset/images/icon/order_cancel.png',
+          navTitle:'订单取消' 
+        }
+      ],
+      mainList:[
+        {
+          urll:'/src/asset/images/icon/bill.png',
+          mainTitle:'我的账单'
+        },
+        {
+          urll:'/src/asset/images/icon/auth.png',
+          mainTitle:'我的授权'
+        },
+        {
+            urll:'/src/asset/images/icon/about.png',
             mainTitle:'关于我们'
         },
       ]  
@@ -62,15 +74,23 @@ var vm = new Vue({
     filters: {
     },
     methods: {
-    //   myFunction() {
-    //     document.getElementById("demo").innerHTML = x += 1;
-    //  }
+      hanfnn(){
+        $('.main-list1').hide();
+        $('.main-list').show();
+        $('#page-back').addClass('hover').siblings('#page-front').removeClass('hover')
+      },
+      hanfnn1(){
+        alert(123)
+        $('.main-list').hide();
+        $('.main-list1').show();
+      $('#page-front').addClass('hover').siblings('#page-back').removeClass('hover')
+      }
     },
     created() {
     },
     destroyed() {
     },
-    mounted() {
+    mounted:function() {
         var body = document.body.clientWidth;		
         document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
     },
