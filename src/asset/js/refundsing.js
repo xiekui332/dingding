0acd011bf0bc82be49303f0eb2a058fde07aa76d
zhuyl <zhuyl@cnjiang.com>
2018-04-28 21:35:08 +0800

    退货说明的静态页面

---
+++ src/asset/js/refundsing.js
@@ -0,0 +1,22 @@
+var vm = new Vue({
+    el: '#app',
+    components: {},
+    props: {
+      
+    },
+    data: {
+    },
+    computed: {},
+    watch: {},
+    filters: {},
+    methods: {
+      
+    },
+    created() {},
+    destroyed() {},
+    mounted: function () {
+      var body = document.body.clientWidth;
+      document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
+      
+    },
+  })
