9384440da2f8ee2bc0ac8873292c7a75963b83a9
xk <18334794858@163.com>
2018-05-03 15:54:47 +0800

    整理账单代码

--- src/asset/js/billOrder.js
+++ src/asset/js/billOrder.js
@@ -3,10 +3,8 @@ var vm = new Vue({
 	//	此处的data返回应该是一个object，vue-cli构建的里面才可返回方法
 	data: {
 		orderId: 202,
-		list: [
-		],
-		list2: [
-		],
+		noPaidList: [],
+		paidList: [],
 		ordersProductEntity: {},
 		allCount: '',
 		waitMoney: '',
@@ -34,15 +32,15 @@ var vm = new Vue({
 							this.money = 0;
 						}
 						this.ordersProductEntity.totalAmount = data.data.ordersProductEntity.totalAmount.toFixed(2);
-						this.list = data.data.noPaidList;
-						this.list2 = data.data.paidList;
+						this.noPaidList = data.data.noPaidList;
+						this.paidList = data.data.paidList;
 						this.money = data.data.money;
 						this.waitMoney = (this.ordersProductEntity.totalAmount - this.money).toFixed(2);
-						this.list.forEach((k, v) => {
+						this.noPaidList.forEach((k, v) => {
 							k.cdate = getTime(k.cdate, 1);
 							this.allCount = data.data.ordersProductEntity.periodNumber;
 						});
-						this.list2.forEach((k, v) => {
+						this.paidList.forEach((k, v) => {
 							k.cdate = getTime(k.cdate);
 						})
 					} else {
@@ -53,10 +51,6 @@ var vm = new Vue({
 					ddToast('网络错误')
 				}
 			})
-
-			// this.$axios.get(url+'shop-service/rest/bills/Ddview',{'orderId':''}).then(res=>{
-			// 	console.log(res)
-			// })
 		}
 	},
 	mounted() {
