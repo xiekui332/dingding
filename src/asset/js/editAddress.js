ff430afc4a9381b547c79fb16bdbd5e137381db5
wzz <1372633952@qq.com>
2018-05-03 09:59:23 +0800

    update: 编辑地址

--- src/asset/js/editAddress.js
+++ src/asset/js/editAddress.js
@@ -1,72 +1,91 @@
 var vm = new Vue({
-    el:'#app',
-    //	此处的data返回应该是一个object，vue-cli构建的里面才可返回方法
-    data:{
-        address: {
-            nailUserId: '',
-            nailUserInfoId: '',
-            username: '',
-            mobile: '',
-            company: '',
-            provinceId: 0,
-            cityId: 0,
-            districtId: 0,
-            detail: '',
-            isDefault: 0,
-            address: ''
-        },
+	el: '#app',
+	data: {
+		address: {
+			nailUserId: '1',
+			nailUserInfoId: '1',
+			username: '',
+			mobile: '',
+			company: '',
+			provinceId: 0,
+			cityId: 0,
+			districtId: 0,
+			detail: '',
+			// isDefault: 0,
+			address: ''
+		},
 
-        // 地址
-        // address: '',
-        addressShow: false,
-        addresslist: [],
-        addressVisible: false,
-        addressList: [{name:'浙江省', id:'330903'},{name:'杭州市', id:'330903'},{name:'西湖区', id:'330903'}],
-        province: province
-    },
-    methods:{
-        saveAddress(){
-        },
-        addressChange(addressList) {
-            this.address.address = ''
-            addressList.forEach(address => {
-                this.address.address += address.name + ' '
-            });
-        },
-        // 地址
-        getDetaiAddress(){
-            let citycode = ''
-            // if (this.selectedProvince == ""){
-            //     citycode = ''
-            // }else {
-            //     this.user.cityId = getId(citytest, this.addressId[1]);
-            //     citycode = this.user.cityId;
-            // }
-            console.log(citycode);
-            let key = this.address.detailAddress;
-            let autocomplete = ''
-            AMap.plugin(['AMap.Autocomplete','AMap.PlaceSearch'],()=>{
-                var autoOptions = {
-                    city: citycode //城市，默认全国
-                };
-                autocomplete= new AMap.Autocomplete(autoOptions);
-                autocomplete.search(key, (status, result)=>{
-                    //TODO:开发者使用result自己进行下拉列表的显示与交互功能
-                        // console.log(key)
-                        // console.log(result)
-                        // console.log(this);
-                        this.addresslist = result.tips;
-                    });
-            });
-            this.addressShow = true
-        },
-        looseBlur(){
-            this.addressShow = false
-        },
-        chooseDetailAddress(item){
-            this.address.detailAddress = item.name;
-            this.addressShow = false;
-        },
-    }
+		// 地址
+		// address: '',
+		addressShow: false,
+		detailAddresslist: [],
+		addressVisible: false,
+		addressList: [{ name: '浙江省', id: '330903' }, { name: '杭州市', id: '330903' }, { name: '西湖区', id: '330903' }],
+		province: province
+	},
+	methods: {
+		saveAddress() {
+			let url = getApiUrl('/api/address/add_shipping_address')
+			$.ajax({
+                url: url,
+                type: "POST",
+                dataType: "json",
+                data: this.address,
+                xhrFields: {
+                    withCredentials: true
+                },
+                crossDomain: true,
+                success: result => {
+                    if (result.code == 200) {
+                        location.href = 'manageAddress.html'
+                    } else {
+                        ddToast(result.message)
+                    }
+                },
+                error: e => {
+                    ddToast('网络错误')
+				}
+			})
+		},
+		addressChange(addressList) {
+			this.address.address = ''
+			addressList.forEach(address => {
+				this.address.address += address.name + ' '
+			});
+			this.address.provinceId = addressList[0].id
+			this.address.cityId = addressList[1].id
+			this.address.districtId = addressList[2].id
+		},
+		// 地址
+		getDetaiAddress() {
+			let citycode = ''
+			if (this.address.provinceId == ""){
+			    citycode = ''
+			}else {
+			    citycode = this.address.cityId;
+			}
+			// console.log(citycode);
+			let key = this.address.detail;
+			let autocomplete = ''
+			AMap.plugin(['AMap.Autocomplete', 'AMap.PlaceSearch'], () => {
+				var autoOptions = {
+					city: citycode //城市，默认全国
+				};
+				autocomplete = new AMap.Autocomplete(autoOptions);
+				autocomplete.search(key, (status, result) => {
+					//TODO:开发者使用result自己进行下拉列表的显示与交互功能
+					this.detailAddresslist = result.tips;
+				});
+			});
+			this.addressShow = true
+		},
+		looseBlur() {
+			this.addressShow = false
+		},
+		chooseDetailAddress(item) {
+			this.address.detail = item.name;
+			this.addressShow = false;
+		},
+	}
 })
 
