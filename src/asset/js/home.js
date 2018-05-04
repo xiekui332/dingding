d04b52ac0c500a853f9d597547cf9a981e8cde50
wzz <1372633952@qq.com>
2018-05-04 19:00:24 +0800

    test

--- src/asset/js/home.js
+++ src/asset/js/home.js
@@ -44,23 +44,23 @@ var vm = new Vue({
       
 
       // let url = 'http://api.taozugong.com:8080/ding-isv-access/suite/callback/suitepfxkjqyuxbyxkeqw'
-      // $.ajax({
-      //   type: "POST",
-      //   url: 'http://api.taozugong.com:8080/ding-isv-access/suite/callback/suitepfxkjqyuxbyxkeqw',
-      //   xhrFields:{
-      //   	withCredentails:true
-      //   },
-      //   crossDomain:true,
-      //   data: {
-      //   },
-      //   success: (res) => {
-      //     alert(JSON.stringify(res))
+      $.ajax({
+        type: "POST",
+        url: 'http://api.taozugong.com:8080/ding-isv-access/suite/callback/suitepfxkjqyuxbyxkeqw',
+        xhrFields:{
+        	withCredentails:true
+        },
+        crossDomain:true,
+        data: {
+        },
+        success: (res) => {
+          alert(JSON.stringify(res))
 
-      //     let authCode = requestAuthCode(corpid);
+          let authCode = requestAuthCode(corpid);
 
 
-      //   }
-      // })
+        }
+      })
 
 
       let corpid = 'ding232f30042c7d834635c2f4657eb6378f'
