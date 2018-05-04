f7651d781ef27224fb0cdb3286e6dd55820d2433
wzz <1372633952@qq.com>
2018-05-04 21:15:49 +0800

    test

--- src/asset/js/home.js
+++ src/asset/js/home.js
@@ -38,51 +38,57 @@ var vm = new Vue({
       location.href = 'goodsDetail.html?productId=' + productId
     },
     getAuthCode() {
-      alert(8)
-      // let url = getApiUrl('/ding-isv-access/get_js_config');
+      let url = getApiUrl('/ding-isv-access/get_js_config');
 
       // let url = 'http://api.taozugong.com:8080/ding-isv-access/get_js_config';
       
-
-      // let url = 'http://api.taozugong.com:8080/ding-isv-access/suite/callback/suitepfxkjqyuxbyxkeqw'
-
-      // $.ajax({
-			// 	url: url,
-			// 	type: "GET",
-			// 	dataType: "json",
-			// 	data: {
-      //     url: window.location.href,
-      //     corpId: 'dingaaa4a95c02214e0835c2f4657eb6378f'
-      //   },
-			// 	xhrFields: {
-			// 		withCredentials: true
-			// 	},
-			// 	crossDomain: true,
-			// 	success: res => {
-      //     alert(9)
-      //     alert("success:"+JSON.stringify(res))
-			// 	},
-			// 	error: e => {
-			// 		alert("error:"+JSON.stringify(e))
-			// 	}
-			// })
-
-      let corpid = 'ding232f30042c7d834635c2f4657eb6378f'
-      // let authCode = requestAuthCode(corpid);
-      // alert('authCode:'+authCode)
-
-      // alert(2)
-      dd.runtime.permission.requestAuthCode({
-        corpId: corpid,
-        onSuccess: function(result) {
-
-            alert('requestAuthCode:'+JSON.stringify(result))
-            return result.code
+      // let corpId = getUrlParam('corpIp')
+      let corpId = 'ding232f30042c7d834635c2f4657eb6378f'
+      
+      $.ajax({
+				url: url,
+				type: "GET",
+				dataType: "json",
+				data: {
+          url: window.location.href,
+          corpId: corpId
         },
-        onFail : function(err) {
-            alert(1)
-        }
-      })
+				xhrFields: {
+					withCredentials: true
+				},
+				crossDomain: true,
+				success: res => {
+          alert("success:"+JSON.stringify(res))
+
+          dd.config({
+              agentId: res.agentId, // 必填，微应用ID
+              corpId: res.orpId,//必填，企业ID
+              timeStamp: res.timeStamp, // 必填，生成签名的时间戳
+              nonceStr: res.nonce, // 必填，生成签名的随机串
+              signature: res.signature, // 必填，签名
+              jsApiList: ['ui.pullToRefresh.enable','ui.pullToRefresh.stop','biz.util.openLink','biz.navigation.setLeft','biz.navigation.setTitle','biz.navigation.setRight'] // 必填，需要使用的jsapi列表
+          });
+
+          dd.ready(function(){
+
+
+                dd.runtime.permission.requestAuthCode({
+                    corpId: corpId,
+                    onSuccess: function(result) {
+            
+                        alert('requestAuthCode:'+JSON.stringify(result))
+                        return result.code
+                    },
+                    onFail : function(err) {
+                        alert("fail"+1)
+                    }
+                  })
+              });
+				},
+				error: e => {
+					alert("error:"+JSON.stringify(e))
+				}
+			})
     },
     getUserId() {
       alert(1)
@@ -94,7 +100,7 @@ var vm = new Vue({
         onFail: function (err) {
             alert('userGet fail: ' + JSON.stringify(err));
         }
-    });
+      });
     }
   },
   created() {},
