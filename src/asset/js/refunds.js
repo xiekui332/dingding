f648cbe72096ed72cf323c9fb3182affa4307e7f
xk <18334794858@163.com>
2018-05-04 16:48:31 +0800

    对接确认归还接口，未完

--- src/asset/js/refunds.js
+++ src/asset/js/refunds.js
@@ -11,15 +11,34 @@ var vm = new Vue({
     watch: {},
     filters: {},
     methods: {
+//  	确认归还按钮
       toRefundsing(){
         this.showA=false;
-      }
+      },
+//    页面加载函数
+		init(){
+			$.ajax({
+				type:"post",
+				url:getApiUrl("/nail/requestreturn.html"),
+				async:true,
+				data:{
+					order_no:'20'
+				},
+				xhrFields:{
+					withCredentials:true
+				},
+				crossDomain:true,
+				success:res =>{
+					console.log(res)
+				}
+			});
+		}
     },
     created() {},
     destroyed() {},
     mounted: function () {
       var body = document.body.clientWidth;
       document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
-      
+      this.init();
     },
   })
