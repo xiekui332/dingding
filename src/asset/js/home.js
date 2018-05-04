a2c09997a307b6c2535a9c86e9ed64e7e90edc4f
wzz <1372633952@qq.com>
2018-05-04 19:01:43 +0800

    update: test

--- src/asset/js/home.js
+++ src/asset/js/home.js
@@ -44,28 +44,45 @@ var vm = new Vue({
       
 
       // let url = 'http://api.taozugong.com:8080/ding-isv-access/suite/callback/suitepfxkjqyuxbyxkeqw'
-      $.ajax({
-        type: "POST",
-        url: 'http://api.taozugong.com:8080/ding-isv-access/suite/callback/suitepfxkjqyuxbyxkeqw',
-        xhrFields:{
-        	withCredentails:true
-        },
-        crossDomain:true,
-        data: {
-        },
-        success: (res) => {
-          alert(JSON.stringify(res))
+      // $.ajax({
+      //   type: "POST",
+      //   url: 'http://api.taozugong.com:8080/ding-isv-access/suite/callback/suitepfxkjqyuxbyxkeqw',
+      //   xhrFields:{
+      //   	withCredentails:true
+      //   },
+      //   crossDomain:true,
+      //   data: {
+      //   },
+      //   success: (res) => {
+      //     alert(JSON.stringify(res))
 
-          let authCode = requestAuthCode(corpid);
+      //     let authCode = requestAuthCode(corpid);
 
 
-        }
-      })
+      //   }
+      // })
 
 
       let corpid = 'ding232f30042c7d834635c2f4657eb6378f'
       let authCode = requestAuthCode(corpid);
       alert('authCode:'+authCode)
+
+
+
+      // dd.runtime.permission.requestAuthCode({
+      //   corpId: corpid,
+      //   onSuccess: function(result) {
+
+      //       alert('requestAuthCode:'+JSON.result)
+      //       return result.code
+      //   /*{
+      //       code: 'hYLK98jkf0m' //string authCode
+      //   }*/
+      //   },
+      //   onFail : function(err) {
+      //       alert(1)
+      //   }
+      // })
     }
   },
   created() {},
